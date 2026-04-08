73fdcd994f76fb2ccc6b2bc65a50d8af5916ac4d
Smasduq <sadiqulmasduq2010@gmail.com>
2026-04-08 15:20:47 +0100

    changed /frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  envDir: '../',
-  plugins: [react()],
-  server: {
-    port: 5175,
-    host: '0.0.0.0'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    optimizeDeps: {
+      include: ['react-paystack']
+    },
+    server: {
+      proxy: {
+        '/api/v1/': {
+          target: env.VITE_API_URL || 'http://localhost:8000',
+          changeOrigin: true
+        },
+        '/api/rust/': {
+          target: env.VITE_RUST_API_URL || 'http://localhost:8081',
+          changeOrigin: true
+        }
+      }
+    }
   }
 })
